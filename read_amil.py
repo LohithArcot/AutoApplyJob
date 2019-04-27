325ab1eb6dd154b4e805b42ce350980618152689
lohithpro <lohith.arcot@gmail.com>
2019-04-27 21:32:23 +0530

    initial changes

---
+++ read_amil.py
@@ -0,0 +1,126 @@
+from __future__ import print_function
+
+import os.path
+import pickle
+
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+
+def authenticate():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+
+    # Call the Gmail API
+    # results = service.users().labels().list(userId='me').execute()
+    # labels = results.get('labels', [])
+    #
+    # if not labels:
+    #     print('No labels found.')
+    # else:
+    #     print('Labels:')
+    #     for label in labels:
+    #         print(label['name'])
+    return service
+
+
+"""Get a list of Messages from the user's mailbox."""
+
+from apiclient import errors
+
+
+def ListMessagesMatchingQuery(service, user_id, query=''):
+    """List all Messages of the user's mailbox matching the query.
+
+    Args:
+      service: Authorized Gmail API service instance.
+      user_id: User's email address. The special value "me"
+      can be used to indicate the authenticated user.
+      query: String used to filter messages returned.
+      Eg.- 'from:user@some_domain.com' for Messages from a particular sender.
+
+    Returns:
+      List of Messages that match the criteria of the query. Note that the
+      returned list contains Message IDs, you must use get with the
+      appropriate ID to get the details of a Message.
+    """
+    try:
+        response = service.users().messages().list(userId=user_id,
+                                                   q=query).execute()
+        messages = []
+        if 'messages' in response:
+            messages.extend(response['messages'])
+
+        while 'nextPageToken' in response:
+            page_token = response['nextPageToken']
+            response = service.users().messages().list(userId=user_id, q=query,
+                                                       pageToken=page_token).execute()
+            messages.extend(response['messages'])
+
+        return messages
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+
+
+def ListMessagesWithLabels(service, user_id, label_ids=[]):
+    """List all Messages of the user's mailbox with label_ids applied.
+
+    Args:
+      service: Authorized Gmail API service instance.
+      user_id: User's email address. The special value "me"
+      can be used to indicate the authenticated user.
+      label_ids: Only return Messages with these labelIds applied.
+
+    Returns:
+      List of Messages that have all required Labels applied. Note that the
+      returned list contains Message IDs, you must use get with the
+      appropriate id to get the details of a Message.
+    """
+    try:
+        response = service.users().messages().list(userId=user_id,
+                                                   labelIds=label_ids).execute()
+        messages = []
+        if 'messages' in response:
+            messages.extend(response['messages'])
+
+        while 'nextPageToken' in response:
+            page_token = response['nextPageToken']
+            response = service.users().messages().list(userId=user_id,
+                                                       labelIds=label_ids,
+                                                       pageToken=page_token).execute()
+            messages.extend(response['messages'])
+
+        return messages
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+
+
+service_obj = authenticate()
+labels = service_obj.users().labels().list(userId='me').execute()
+unreadMsgs = ListMessagesWithLabels(service_obj, 'me', ['UNREAD'])
+msg = service_obj.users().messages().get(id = '16a5ccf0bca92a70',userId = 'me').execute()
